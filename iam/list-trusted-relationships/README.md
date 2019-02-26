a7bce35f38da50057e4e71cfe792bb77a51eac98
Matthew Lowe <matthew.lowe@iis.se>
2019-02-26 21:36:27 +0100

    Finished list-role-trusted-relationships

---
+++ iam/list-trusted-relationships/README.md
@@ -0,0 +1,41 @@
+# List Trusted Relationships
+Prints role names and the trusted relationships for the provided `profile`.
+
+### Requirements
+
++ Your `profile` credentials must allow you to `iam:ListRoles`.
+
++ `boto3` must be installed.
+
+### Usage
+
+`./list-trusted-relationships {profile}`
+
+Where `profile` is the name of the credentials you want to use.
+
+### Output
+
+Output is in `yaml`:
+
+```yaml
+Roles:
+  <role_name>:
+    PrincipalTypes:
+      <principal_type>:
+      - [<item1>, <item2>, <item3>]
+```
+
+Example output:
+
+```yaml
+Roles:
+  rds-monitoring-role:
+    PrincipalTypes:
+      Service:
+      - [monitoring.rds.amazonaws.com, rds.amazonaws.com]
+  developer-role:
+    PrincipalTypes:  
+      AWS:
+      - ['arn:aws:iam::12345678910:root', 'arn:aws:iam::12345678910:root']
+```
+
---
+++ iam/list-trusted-relationships/list-trusted-relationships.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import argparse
+import boto3
+import yaml
+
+
+class Data:
+    def __init__(self):
+        self.data = {'Roles': {}}
+        pass
+
+    def add_role(self, role_name):
+        self.data['Roles'][role_name] = {}
+
+    def add_principal_type(self, role_name, principal_type):
+        self.data['Roles'][role_name] = {'PrincipalTypes': {}}
+        self.data['Roles'][role_name]['PrincipalTypes'][principal_type] = []
+
+    def add_principal(self, role_name, principal_type, principal):
+        self.data['Roles'][role_name]['PrincipalTypes'][principal_type].append(principal)
+
+    def print(self):
+        print(yaml.dump(self.data))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('profile', help='AWS credentials profile to use')
+    args = parser.parse_args()
+
+    iam = boto3.session.Session(profile_name=args.profile).client('iam')
+    roles = iam.list_roles()
+    data = Data()
+
+    while True:
+        for role in roles.get('Roles', []):
+            data.add_role(role['RoleName'])
+            if not role['AssumeRolePolicyDocument']:
+                pass
+            statements = role['AssumeRolePolicyDocument'].get('Statement', [])
+            for statement in statements:
+                for principal_type in statement['Principal']:
+                    data.add_principal_type(role['RoleName'], principal_type)
+                    data.add_principal(role['RoleName'],
+                                       principal_type,
+                                       statement['Principal'][principal_type])
+
+        if not roles['IsTruncated']:
+            break
+        roles = iam.list_roles(Marker=roles['Marker'])
+
+    data.print()
